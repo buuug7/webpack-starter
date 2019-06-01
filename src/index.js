157a59a08abbc9325c136f3bb10e1638b6812173
buuug7 <youpp@126.com>
2019-06-01 17:59:11 +0800

    update dependencies

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 import '@babel/polyfill';
 import './app.scss';
+
+let h2 = document.createElement('h2');
+h2.textContent = "Hello World";
+h2.classList.add('hello-world');
+document.body.append(h2);
