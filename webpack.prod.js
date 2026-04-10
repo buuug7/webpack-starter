91a242b21944e14cb329ad62ab3dd875c7b408a3
buuug7 <youpp@126.com>
2026-04-10 23:15:20 +0800

    chore: update dependencies and improve webpack configuration
    - Updated devDependencies in package.json to latest versions for better performance and security.
    - Added webpack.common.js for shared configuration between development and production.
    - Modified webpack.dev.js to use the new static property for serving content.
    - Fixed typo in webpack.prod.js to correctly reference webpack.common.js.

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,5 @@
 const { merge } = require('webpack-merge');
-const common = require('./wepack.common.js');
+const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'production',
